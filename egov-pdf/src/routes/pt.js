4ea92453307aea2a03aeeaf290192ae746b8bc23
nvcs057 <nvcs057@gmail.com>
2021-04-10 17:37:29 +0530

    added a new adapter

--- egov-pdf/src/routes/pt.js
+++ egov-pdf/src/routes/pt.js
@@ -198,6 +198,113 @@ router.post(
   })
 );
 
+
+router.post(
+  "/ptbillTest",
+  asyncMiddleware(async function (req, res, next) {
+    var tenantId = req.query.tenantId;
+    var propertyId = req.query.propertyId;
+    var requestinfo = req.body;
+    if (requestinfo == undefined) {
+      return renderError(res, "requestinfo can not be null", 400);
+    }
+    if (!tenantId || !propertyId) {
+      return renderError(
+        res,
+        "tenantId and uuid are mandatory to generate the ptbill",
+        400
+      );
+    }
+    try {
+      try {
+        resProperty = await search_property_with_propnumber(
+          propertyId,
+          tenantId,
+          requestinfo
+        );
+      } catch (ex) {
+        console.log(ex.stack);
+        if (ex.response && ex.response.data) console.log(ex.response.data);
+        return renderError(res, "Failed to query details of the property", 500);
+      }
+      var properties = resProperty.data;
+     // console.log("properties--",properties);
+      if (
+        properties &&
+        properties.Properties &&
+        properties.Properties.length > 0
+      ) {
+        var propertyid = properties.Properties[0].propertyId;
+        var billresponse;
+        try {
+          billresponse = await search_bill(propertyid, tenantId, requestinfo);
+        } catch (ex) {
+          console.log(ex.stack);
+          if (ex.response && ex.response.data) console.log(ex.response.data);
+          return renderError(res, `Failed to query bills for property`, 500);
+        }
+        var bills = billresponse.data;
+        bills.Bills[0].billDetails.sort(function(x,y){
+          return x.fromPeriod - y.fromPeriod
+        })
+        bills.Bills[0].billDetails.map(function(x){
+           x.billAccountDetails.sort(function(x,y){
+              return x.order - y.order
+           })
+        })
+        let temp =[];
+        bills.Bills[0].billDetails[0].billAccountDetails.map(function(x){
+          let obj = {}
+          obj.taxHeadCode = x.taxHeadCode;
+          obj.currentDemand = x.amount
+          temp.push(obj);
+        })
+        bills.Bills[0].billDetails.splice(0,1); //removed the zero obj
+        // write from here
+        console.log(JSON.stringify(temp));
+        //console.log("bills--",JSON.stringify(bills));
+        if (bills && bills.Bills && bills.Bills.length > 0) {
+          var pdfResponse;
+          var pdfkey = config.pdf.ptbill_pdf_template;
+          try {
+            var billArray = { Bill: bills.Bills };
+            pdfResponse = await create_pdf(
+              tenantId,
+              pdfkey,
+              billArray,
+              requestinfo
+            );
+          } catch (ex) {
+            console.log(ex.stack);
+            if (ex.response && ex.response.data) console.log(ex.response.data);
+            return renderError(res, "Failed to generate PDF for property", 500);
+          }
+
+          var filename = `${pdfkey}_${new Date().getTime()}`;
+
+          //pdfData = pdfResponse.data.read();
+          res.writeHead(200, {
+            "Content-Type": "application/pdf",
+            "Content-Disposition": `attachment; filename=${filename}.pdf`,
+          });
+          pdfResponse.data.pipe(res);
+        } else {
+          return renderError(res, "There is no bill for this id", 404);
+        }
+      } else {
+        return renderError(
+          res,
+          "There is no property for you for this id",
+          404
+        );
+      }
+    } catch (ex) {
+      console.log(ex.stack);
+    }
+  })
+);
+
+
 router.post(
   "/ptreceipt",
   asyncMiddleware(async function (req, res, next) {
